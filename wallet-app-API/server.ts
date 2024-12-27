fa057346d6029003c1b1b3b4e621578cd9f205c5
Ruslan <rsekirin@prendehealth.com>
2024-12-27 02:05:12 +0200

    fix build

--- wallet-app-API/server.ts
+++ wallet-app-API/server.ts
@@ -3,13 +3,13 @@ import fs from "fs";
 import path from "path";
 import cors from "cors";
 import { fileURLToPath } from "url";
-import dotenv from "dotenv";
 
-dotenv.config();
 const app = express();
 app.use(cors());
+app.use(express.json());
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const dataPath = path.resolve(__dirname, "data/data.json");
+const dataPath = path.resolve(__dirname, "../data/data.json");
 
 app.get("/api/transactions", (req, res) => {
   fs.readFile(dataPath, "utf-8", (err, data) => {
@@ -17,7 +17,7 @@ app.get("/api/transactions", (req, res) => {
       res.status(500).send("Error reading data");
       return;
     }
-    res.send(JSON.parse(data));
+    res.json(JSON.parse(data));
   });
 });
 
@@ -29,7 +29,6 @@ app.get("/api/transactions/:id", (req, res) => {
       res.status(500).send("Error reading data");
       return;
     }
-
     const transactions = JSON.parse(data);
     const transaction = transactions.find((t: any) => t.id === transactionId);
 
@@ -40,6 +39,7 @@ app.get("/api/transactions/:id", (req, res) => {
     }
   });
 });
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
